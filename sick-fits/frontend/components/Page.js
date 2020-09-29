3bb3c54c54ffd7058271d4180184002e23b678e9
Lingxiao Wang <xwangl213@gmail.com>
2020-09-29 18:55:17 +1000

    Added meta

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,14 +1,15 @@
-
-import React, { Component } from 'react'
-import Header from '../components/Header'
+import React, { Component } from "react";
+import Header from "../components/Header";
+import Meta from "./Meta";
 
 export default class Page extends Component {
   render() {
     return (
       <div>
+        <Meta />
         <Header />
         <p>Pae compamge</p>
       </div>
-    )
+    );
   }
 }
--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,18 +1,18 @@
-import App, { container } from 'next/app'
-import Page from '../componets/Page'
+import App, { Container } from "next/app";
+import Page from "../components/Page";
 
 class MyApp extends App {
   render() {
-    const { Component } = this.props
+    const { Component } = this.props;
 
     return (
       <Container>
         <Page>
-          <Component />        
+          <Component />
         </Page>
       </Container>
-    )
+    );
   }
 }
 
-export default MyApp
+export default MyApp;
